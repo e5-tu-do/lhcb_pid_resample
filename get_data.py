412e0e6888e24d3c138fa697d7133aa9cf4ffce9
Igor Babuschkin <igor@babuschk.in>
2015-01-22 18:37:09 +0100

    Simplify script

--- get_data.py
+++ get_data.py
@@ -19,20 +19,17 @@ def wrap_iter(it):
 def get_data(data):
     from numpy import zeros
     curr = data.get(0)
-    ret = zeros((curr.getSize(), data.numEntries()))
+    ret = zeros((data.numEntries(), curr.getSize()))
     vars = []
     for v in wrap_iter(curr.iterator()):
         vars.append(v)
-    idx = 0
-    while curr:
-        for i, v in enumerate(vars):
-            var = curr.find(v)
-            try:
-                ret[i, idx] = var.getVal()
-            except AttributeError:
-                ret[i, idx] = var.getIndex()
-        idx += 1
+    for idx in range(data.numEntries()):
         curr = data.get(idx)
+        for i, var in enumerate(wrap_iter(curr.iterator())):
+            try:
+                ret[idx, i] = var.getVal()
+            except AttributeError:
+                ret[idx, i] = var.getIndex()
     return ret, map(lambda x: x.GetName(), vars)
 
 with open('raw_file_locations.json') as f:
@@ -47,7 +44,7 @@ for sample in locations:
         data = ws.allData().front()
         logging.info('Converting data')
         table, vars = get_data(data)
-        df = DataFrame(table.T, columns=vars)
+        df = DataFrame(table, columns=vars)
         logging.info('Saving data to {}'.format(output))
         df.to_root(output)
 
